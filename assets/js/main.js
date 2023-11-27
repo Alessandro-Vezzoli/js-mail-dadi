d664c5ee9757519777f74f7635e22237b5a720a2
VEZZO\alexv <alessandro.vezzoli@vp360.eu>
2023-11-28 00:24:17 +0100

    start code

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+/* 
+---Mail
+----------Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+---Gioco dei dadi
+----------Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+*/
+
+//Chiedere all'utente la mail
+
+//Controllare che sia nella lista
+
+//Generare un numero random da 1 a 6 per il giocatore
+
+//Cenerare un numero random da 1 a 6 per il computer
+
+//Cedere quale sei due numeri è più alto
+
+//Stabilire il vincitore
